8ca39b01c78274ea43f96b8c5c99abff2d7c0a12
vagrant <vagrant@localhost.localdomain>
2016-02-29 23:44:40 +0900

    bootstrap作成

--- bootstrap.php
+++ bootstrap.php
@@ -3,9 +3,9 @@
 ブートストラップ: 「自動実行される」的な意味
 
 1. クラスオートロードを行うClassLoaderクラスのインスタンス化をする
-2. ClassLoader::regDirectory()の呼び出し
+2. ClassLoader::registerDir()の呼び出し
     - カレントディレクトリに /mvc を付加したパスと /models を付加したパスを作成
-    - 上記を引数問sて ClassLoaderクラス の regDirectory() を呼び出す
+    - 上記を引数問sて ClassLoaderクラス の registerDir() を呼び出す
     - これにより ClassLoaderクラス の $dirsプロパティ にオートロード対象のディレクトリのパスが格納される
 3. ClassLoader::register()の呼び出し
     - ClassLoaderインスタンス に loadClass()メソッドがコールバックとして登録
@@ -18,8 +18,8 @@ require 'core/ClassLoader.php';
 
 $ClassLoader = new ClassLoader();
 
-$ClassLoader->regDirectory(dirname(__FILE__) . '/core'); // coreディレクトリを登録
+$ClassLoader->registerDir(dirname(__FILE__) . '/core'); // coreディレクトリを登録
 
-$ClassLoader->regDirectory(dirname(__FILE__) . '/models'); // models ディレクトリを登録
+$ClassLoader->registerDir(dirname(__FILE__) . '/models'); // models ディレクトリを登録
 
 $ClassLoader->register(); // 上記ディレクトリをオートロード対象に登録
