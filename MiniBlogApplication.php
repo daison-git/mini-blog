c0d60784679d4638ba56a53063b01a375dc7103b
vagrant <vagrant@localhost.localdomain>
2016-03-05 17:45:01 +0900

    indexアクション

--- MiniBlogApplication.php
+++ MiniBlogApplication.php
@@ -10,10 +10,10 @@ class MiniBlogApplication extends Application {
 
     protected function registerRoutes() {
         return array(
-            // '/'
-            //     => array('controller' => 'status', 'action' => 'index'),
-            // '/status/post'
-            //     => array('controller' => 'status', 'action' => 'post'),
+            '/'
+                => array('controller' => 'status', 'action' => 'index'),
+            '/status/post'
+                => array('controller' => 'status', 'action' => 'post'),
             // '/user/:user_name'
             //     => array('controller' => 'status', 'action' => 'user'),
             // '/user/:user_name/status/:id'
