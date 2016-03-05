0473f9d94497f2f2456bf15ab6f566f380f0e616
vagrant <vagrant@localhost.localdomain>
2016-03-05 18:37:06 +0900

    postアクション

--- controllers/StatusController.php
+++ controllers/StatusController.php
@@ -3,7 +3,7 @@
 class StatusController extends Controller {
 
     public function indexAction() {
-        $user = $this->session->get('User');
+        $user = $this->session->get('user');
         $statuses = $this->db_manager->get('Status')->fetchAllPersonalArchivesByUserId($user['id']);
 
         return $this->render(array(
@@ -11,6 +11,45 @@ class StatusController extends Controller {
             'body'     => '',
             '_token'   => $this->generateCsrfToken('status/post'),
             ));
+    }
+
+    public function postAction() {
+        if (!$this->request->isPost()) {
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+        if (!$this->checkCsrfToken('status/post', $token)) {
+            return $this->redirect('/');
+        }
+
+        $body = $this->request->getPost('body');
+
+        $errors = array();
+
+        if (!strlen($body)) {
+            $errors[] = 'ひとことを入力してください';
+        } elseif (mb_strlen($body) > 200) {
+            $errors[] = 'ひとことは200文字以内で入力してください';
+        }
+
+        if (count($errors) === 0) {
+            $user = $this->session->get('user');
+            $this->db_manager->get('Status')->insert($user['id'], $body);
+
+            return $this->redirect('/');
+        }
+
+        $user = $this->session->get('user');
+        $statuses = $this->db_manager->get('Status')->fetchAllPersonalArchivesByUserId($user['id']);
+
+        return $this->render(array(
+            'errors'   => $errors,
+            'body'     => $body,
+            'statuses' => $statuses,
+            '_token'   => $this->generateCsrfToken('status/post')
+            ), 'index');
+
     }
 
 }
--- models/StatusRepository.php
+++ models/StatusRepository.php
@@ -11,16 +11,18 @@ class StatusRepository extends DbRepository {
         $stmt = $this->execute($sql, array(
             ':user_id'    => $user_id,
             ':body'       => $body,
-            ':created_at' => $created_at
+            ':created_at' => $now->format('Y-m-d H:i:s')
             ));
     }
 
     public function fetchAllPersonalArchivesByUserId($user_id) {
-        $sql = "SELECT a.* , u.user_name
+        $sql = "
+            SELECT a.* , u.user_name
                  FROM status a
                     LEFT JOIN user u ON a.user_id = u.id
                  WHERE u.id = :user_id
-                 ORDER BY a.created_at DESC";
+                 ORDER BY a.created_at DESC
+            ";
 
         return $this->fetchAll($sql, array(':user_id' => $user_id));
     }
