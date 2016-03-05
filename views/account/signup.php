a86abc40b7be97f4311caa426ff5d3c3b9327c15
vagrant <vagrant@localhost.localdomain>
2016-03-05 17:58:57 +0900

    ユーザ情報入力箇所の分離

--- views/account/signup.php
+++ views/account/signup.php
@@ -8,9 +8,9 @@
     <?php if (isset($errors) && count($errors) > 0) : ?>
         <?php echo $this->render('errors', array('errors' => $errors)); ?>
     <?php endif; ?>
-    <p>ユーザID: <input type="text" name="user_name" value=""></p>
 
-    <p>パスワード: <input type="password" name="password" value=""></p>
-
+    <?php echo $this->render('account/inputs', array(
+        'user_name' => $user_name, 'password' => $password,
+    )); ?>
     <p><input type="submit" value="登録"></p>
 </form>
